129a16942c12cff6bfaec57c05ac1f3027ddeab5
Brian Strobach <6342694+Strobocop@users.noreply.github.com>
2022-01-19 14:58:27 -0600

    Removing unnecessary path and dependency declaration from target.

--- Package.swift
+++ Package.swift
@@ -29,9 +29,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "UIFontLoader",
-            dependencies: [
-            ],
-            path: "Sources/UIFontLoader/",
             resources: [
                 .process("Resources"),
             ]
